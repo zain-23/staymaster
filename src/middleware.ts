874d8972823268cb102e6c04fe62d561b54033d1
Syed zain ali shah <zaingit780@gmail.com>
2024-07-13 01:17:24 +0500

    complte the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { getSession } from "./lib/session";
 
 export async function middleware(request: NextRequest) {
   const session = cookies().get("session")?.value;
-  const authRoutes = ["/sign-in", "sign-up"];
+  const authRoutes = ["/sign-in", "/sign-up"];
   const userRoute = ["/u", "/u/room"];
 
   const currentPath = request.nextUrl.pathname;
@@ -13,10 +13,19 @@ export async function middleware(request: NextRequest) {
     const absoluteUrl = new URL("/sign-in", request.nextUrl);
     return NextResponse.redirect(absoluteUrl);
   }
+  const user = await getSession(session!);
 
   if (session && authRoutes.includes(currentPath)) {
-    const absoluteUrl = new URL("/u", request.nextUrl);
-    return NextResponse.redirect(absoluteUrl);
+    switch (user?.role) {
+      case "admin":
+        const absoluteUrl = new URL("/d", request.nextUrl);
+        return NextResponse.redirect(absoluteUrl);
+
+      default:
+      case "admin":
+        const absoluteUserUrl = new URL("/u", request.nextUrl);
+        return NextResponse.redirect(absoluteUserUrl);
+    }
   }
   // NextResponse.next();
 }
